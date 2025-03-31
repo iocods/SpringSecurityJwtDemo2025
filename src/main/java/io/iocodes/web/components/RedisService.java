f93cf7687afa800f1ba334d28e34d21fd2c58cbb
Isaiah Osarobo <Isaiahosarobo6@gmail.com>
2025-03-31 17:11:33 +0100

    Added a RedisService class and impl and the necessary redis configuration to the yml file

---
+++ src/main/java/io/iocodes/web/components/RedisService.java
@@ -0,0 +1,27 @@
+package io.iocodes.web.components;
+
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisService(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    // Store data with a custom TTL
+    public void setTokenWithTTL(String key, String value, long ttl, TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, value, ttl, timeUnit);
+    }
+
+    // check if token exists in the database
+    public boolean hasToken(String token) {
+        return redisTemplate.hasKey(token);
+    }
+}
+
