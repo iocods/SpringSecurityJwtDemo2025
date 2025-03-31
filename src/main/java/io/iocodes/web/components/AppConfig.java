d9b3cf3f12b30cadc7895e620808945b7659d4d1
Isaiah Osarobo <Isaiahosarobo6@gmail.com>
2025-03-31 21:22:16 +0100

    Added an app config class and defined all necessary beans

--- src/main/java/io/iocodes/web/components/AppConfig.java
+++ src/main/java/io/iocodes/web/components/AppConfig.java
@@ -1,4 +1,49 @@
 package io.iocodes.web.components;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Slf4j
+@Configuration
 public class AppConfig  {
+
+    private final UserRepository userRepository;
+
+    public AppConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username).orElseThrow();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var daoAuthenticationProvider = new DaoAuthenticationProvider(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        try{
+            return config.getAuthenticationManager();
+        } catch (Exception e) {
+            log.info("Error getting authentication manager, Throwing exception again");
+            throw new RuntimeException(e);
+        }
+    }
 }
